419a79c5d219efaf06ef61b67d86ffa149bf0d31
tranthanhtan0511 <vanhongnak@gmail.com>
2024-10-23 19:46:16 +0700

    add suaThongTin

--- ChuongTrinh.cpp
+++ ChuongTrinh.cpp
@@ -3,49 +3,66 @@
 int main()
 {
     NodeNhanVien* dsnv = NULL;
+    NodeNhanVien* id = NULL;  //dung de tim kiem theo id cho nguoi dung
     int a;
     do
     {
-        cout << "\n";
+        system("cls");
         cout << "Danh Sach Menu" << "\n";
         cout << "0. Thoat" << "\n";
         cout << "1. them nhan vien" << "\n";
         cout << "2. xuat danh sach nhan vien" << "\n";
         cout << "3. xoa" << "\n";
         cout << "4. sua" << "\n";
-        cout << "5. tim kiem" << "\n";
-        cout << "6. doc file" << "\n";
-        cout << "7. ghi file" << "\n";
+        cout << "5. tim kiem theo id" << "\n";
+        cout << "6. tim kiem theo chuc vu" << "\n";
+        cout << "7. tim kiem theo ma bang" << "\n";
+        cout << "8. doc file" << "\n";
+        cout << "9. ghi file" << "\n";
         cout << "Nhap so : ";
         cin >> a;
         system("cls");
         switch (a)
         {
         case 0://thoat
-            break;
+            return 0;
         case 1:
             cout << "--------------------\n";
             themDSNhanVien(dsnv);
             cout << "--------------------\n";
-            system("cls");
+            
             break;
         case 2:
+            //cout << "--------------------------\n";
             xuatDSNhanVien(dsnv);
             cout << "--------------------------\n";
-
+            
             break;
         case 3://xoa
             xoaNhanVien(dsnv);
             break;
         case 4://sua
-            subMenu(dsnv);
+            suaThongTinNhanVien(dsnv);
             break;
-        case 5://tim kiem
-            timKiemUser(dsnv);
-
+        case 5://tim kiem theo id
+            id = timKiemUser(dsnv);
+            if (id != NULL)
+            {
+                xuatNhanVien(id);
+            }
+            else
+            {
+                cout << "Khong tim thay nhan vien!"<<endl;
+            }
             break;
-        case 6://doc file
-            if (readFile("quanlynhansu.txt", dsnv))
+        case 6: //tim kiem theo chuc vu
+            timKiemTheoChucVu(dsnv);
+            break;
+        case 7: //tim kiem theo ma bang cap
+            timKiemTheoMaBang(dsnv);
+            break;
+        case 8://doc file
+            if(readFile("danhSachNhanVien.txt", dsnv))
             {
                 cout << "Nhap file thanh cong!" << endl;
             }
@@ -54,8 +71,8 @@ int main()
                 cout << "Doc file that bai!" << endl;
             }
             break;
-        case 7:
-            if (writeFile("quanlynhansu.txt", dsnv))
+        case 9:
+            if (writeFile("danhSachNhanVien.txt", dsnv))
             {
                 cout << "Viet file thanh cong!" << endl;
             }
@@ -65,8 +82,15 @@ int main()
             }
             break;
         default:
-            cout << "nhap lai: " << endl;
+            cout << "nhap lai: "<<endl;
             break;
+        }
+        char check;
+        cout << "Ban co muon tiep tuc khong(y/n): ";
+        cin >> check;
+        if (check == 'n' || check == 'N')
+        {
+            return 0 ;
         }
 
     } while (a != 0);
