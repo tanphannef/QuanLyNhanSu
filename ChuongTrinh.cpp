6e41b86d58dd5f531d884045d267ba9b666e08ce
tranthanhtan0511 <vanhongnak@gmail.com>
2024-10-25 14:33:13 +0700

    Update ChuongTrinh.cpp

--- ChuongTrinh.cpp
+++ ChuongTrinh.cpp
@@ -3,20 +3,21 @@
 int main()
 {
     NodeNhanVien* dsnv = NULL;
-    //NodeNhanVien* id = NULL;  //dung de tim kiem theo id cho nguoi dung
+    NodeNhanVien* id = NULL;  //dung de tim kiem theo id cho nguoi dung
     int a;
     do
     {
         system("cls");
-        cout << "Danh Sach Menu" << "\n";
-        cout << "0. Thoat" << "\n";
-        cout << "1. them nhan vien" << "\n";
-        cout << "2. xuat danh sach nhan vien" << "\n";
-        cout << "3. xoa thong tin nhan vien" << "\n";
-        cout << "4. sua thong tin nhan vien" << "\n";
-        cout << "5. tim kiem" << "\n";
-        cout << "6. doc file" << "\n";
-        cout << "7. ghi file" << "\n";
+        cout << "=========================================================" << endl;
+        cout << "==============|Chuong Trinh Quan Ly Nhan Su|=============" << endl;
+        cout << "======================|Danh Sach Menu|===================" << "\n";
+        cout << "|0. Thoat                                               |" << "\n";
+        cout << "|1. Them Nhan Vien                                      |" << "\n";
+        cout << "|2. Xuat Danh Sach Nhan Vien                            |" << "\n";
+        cout << "|3. Tim Kiem                                            |" << "\n";
+        cout << "|4. Doc File                                            |" << "\n";
+        cout << "|5. Ghi File                                            |" << "\n";
+        cout << "=========================================================" << endl;
         cout << "Nhap so : ";
         cin >> a;
         system("cls");
@@ -24,27 +25,23 @@ int main()
         {
         case 0://thoat
             return 0;
-        case 1://them nhan vien
+        case 1:
             cout << "--------------------\n";
             themDSNhanVien(dsnv);
             cout << "--------------------\n";
+            
             break;
-        case 2://xuat nhan vien
+        case 2:
+            //cout << "--------------------------\n";
             xuatDSNhanVien(dsnv);
             cout << "--------------------------\n";
-
+            
             break;
-        case 3://xoa nhan vien
-            xoaNhanVien(dsnv);
-            break;
-        case 4://sua thong tin nhan vien
-            suaThongTinNhanVien(dsnv);
-            break;
-        case 5://tim kiem nhan su
+        case 3:
             timKiem(dsnv);
             break;
-        case 6://doc file
-            if (readFile("danhSachNhanVien.txt", dsnv))
+        case 4://doc file
+            if(readFile("danhSachNhanVien.txt", dsnv))
             {
                 cout << "Doc file thanh cong!" << endl;
             }
@@ -53,7 +50,7 @@ int main()
                 cout << "Doc file that bai!" << endl;
             }
             break;
-        case 7://ghi file
+        case 5:
             if (writeFile("danhSachNhanVien.txt", dsnv))
             {
                 cout << "Ghi file thanh cong!" << endl;
@@ -64,15 +61,15 @@ int main()
             }
             break;
         default:
-            cout << "nhap lai: " << endl;
+            cout << "nhap lai: "<<endl;
             break;
         }
         char check;
-        cout << "Ban co muon tiep tuc khong(y/n): ";
+        cout << "Ban co muon quay lai menu khong(y/n): ";
         cin >> check;
         if (check == 'n' || check == 'N')
         {
-            return 0;
+            return 0 ;
         }
 
     } while (a != 0);
